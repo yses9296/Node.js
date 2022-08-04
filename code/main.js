ff803081a25c24ad450393ff60e58c554c7fedc8
Eunseo Choi <yses9296@gmail.com>
2022-08-04 23:29:43 +0900

    2022-07-04

--- code/main.js
+++ code/main.js
@@ -1,15 +1,24 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+
+    // console.log(_url);
+    console.log(queryData.name);
+    
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
+    // response.end(fs.readFileSync(__dirname + _url));
 
  
 });
