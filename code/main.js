17850a56e1595f2e35edaa4b8f85910f7cfa5813
Eunseo Choi <yses9296@gmail.com>
2022-08-04 23:33:19 +0900

    2022-08-04

--- code/main.js
+++ code/main.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
