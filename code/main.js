8cae50ac12ddfb4e668eae33a1ae832079f47b9c
Eunseo Choi <yses9296@gmail.com>
2022-08-07 19:48:12 +0900

    2022-08-07

--- code/main.js
+++ code/main.js
@@ -6,43 +6,84 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathName = url.parse(_url, true).pathname;
-    var title = queryData.id;
+    
 
     if(pathName === '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
-        var description = data;
-  
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="index.html">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>
-            ${description}
-          </p>
-        </body>
-        </html>
-        `;
-  
-        response.writeHead(200);
-        response.end(template);
-  
-      });
-    }
+      if(queryData.id === undefined){
+        
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+    
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="index.html">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>
+              ${description}
+            </p>
+          </body>
+          </html>
+          `;
+    
+          response.writeHead(200);
+          response.end(template);
+    
+        });
+      } //queryData.id if-else
+      else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
+          var title = queryData.id;
+          var description = data;
+    
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="index.html">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>
+              ${description}
+            </p>
+          </body>
+          </html>
+          `;
+    
+          response.writeHead(200);
+          response.end(template);
+    
+        });
+
+      }//queryData.id if-else
+
+    } //pathName if-else
     else {
+
       response.writeHead(404);
       response.end('Not Found');
-    }
+
+    } //pathName if-else
+
 
     // if(_url == '/'){
     //   title = 'Welcome';
