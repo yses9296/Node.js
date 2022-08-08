c7f7a684c3e9758758d492a2be2b4aacab83cfd4
Eunseo Choi <yses9296@gmail.com>
2022-08-08 19:30:15 +0900

    2022-08-08

--- code/main.js
+++ code/main.js
@@ -55,35 +55,44 @@ var app = http.createServer(function(request,response){
         });
       } //queryData.id if-else
       else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
-          var title = queryData.id;
-          var description = data;
-    
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="index.html">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>
-              ${description}
-            </p>
-          </body>
-          </html>
-          `;
-    
-          response.writeHead(200);
-          response.end(template);
-    
+        fs.readdir('./data', function(error, filelist){
+          // var title = 'Welcome';
+          // var description = 'Hello, Node.js';
+
+          var list = '<ul>';
+          var i = 0;
+          while( i < filelist.length ){
+            list = list + `<li><a href="/?id=${filelist[i]}"> ${filelist[i]} </li>`
+            i++;
+          }
+          list = list + '</ul>';
+
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
+            var title = queryData.id;
+            var description = data;
+      
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="index.html">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>
+                ${description}
+              </p>
+            </body>
+            </html>
+            `;
+      
+            response.writeHead(200);
+            response.end(template);
+      
+          });
         });
 
       }//queryData.id if-else
